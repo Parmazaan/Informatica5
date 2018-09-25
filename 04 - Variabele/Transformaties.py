a1eee64a8d1cb8e24a22ef27d3b6be2a63599cad
Parmazaan <jelle.parmentier@sgsintpaulus.eu>
2018-09-25 21:36:03 +0200

    IBAN

---
+++ 04 - Variabele/Transformaties.py
@@ -0,0 +1,7 @@
+a = (float(input('a: ')))
+b = (float(input('b: ')))
+
+
+
+print('f(x) = 2(x - 3)^2 + 4')
+print('g(x) = 2(x ' + str(-3 - a)+')^2 + ' + str(4 + b))
---
+++ 04 - Variabele/Wisselgeld.py
@@ -0,0 +1,7 @@
+bedrag = (int(input('bedrag = ')))
+
+uitkomst = (str(bedrag) + ' cent kan je omwisselen in ')
+uitkomst += (str(bedrag // 100 + (bedrag % 100) // 50 + (bedrag % 50) // 20 + (bedrag % 50 % 20) // 10 + (bedrag % 50 % 20 % 10) // 5 + (bedrag % 50 % 20 % 10 % 5) // 2 + (bedrag % 50 % 20 % 10 % 5 % 2) // 1))
+uitkomst += (' muntstukken ')
+
+print(uitkomst)
