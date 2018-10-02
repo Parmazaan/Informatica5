1bcc872f07bcf57d63ccdcdd415b3c51a15b9ef4
Parmazaan <jelle.parmentier@sgsintpaulus.eu>
2018-10-02 19:54:24 +0200

    IBAN

--- 05 - EenvoudigFuncties/Pythagoras.py
+++ 05 - EenvoudigFuncties/Pythagoras.py
@@ -1,7 +1,8 @@
-a = int(input('getal 1: '))
-b = int(input('getal 2: '))
-print('{:>6d}'.format(a * pow(10,i)),'+','{:<6d}'.format(b * pow(10,i)),'=',str((a * pow(10,i)) + (b * pow(10,i))))
-print('{:>6d}'.format(a),'+','{:<6d}'.format(b),'=',str((a) + (b)))
-print('{:>6d}'.format(a * 10),'+','{:<6d}'.format(b * 10),'=',str((a * 10) + (b * 10)))
-print('{:>6d}'.format(a * 10 * 10),'+','{:<6d}'.format(b  * 10 * 10),'=',str((a * 10 * 10) + (b * 10 * 10)))
-print('{:>6d}'.format(a * 10 * 10 * 10),'+','{:<6d}'.format(b * 10 * 10 * 10),'=',str((a * 10 * 10 * 10) + (b * 10 * 10 * 10)))
+from math import sqrt
+
+a = float(input('eerste zijde: '))
+b = float(input('tweede zijde: '))
+
+c = sqrt(pow(a,2) + pow(b,2))
+
+print('In een rechthoekige driekhoek met rechthoekzijden a = '+'{:2.2f}'.format(a)+' en b = '+'{:2.2f}'.format(b)+' is de schuine zijde '+'{:2.2f}'.format(c))
