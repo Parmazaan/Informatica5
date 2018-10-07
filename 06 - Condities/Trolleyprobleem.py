3b21de1fa0630d68cc9bf1c3ca3652b2d643472f
Parmazaan <jelle.parmentier@sgsintpaulus.eu>
2018-10-07 22:25:51 +0200

    crocodileparadox

---
+++ 06 - Condities/Trolleyprobleem.py
@@ -0,0 +1,21 @@
+vraag1 = input('trek je aan de hendel? ')
+
+if vraag1 == 'ja':
+    vraag2 = input('duw je de man van de brug? ')
+    if vraag2 == 'ja':
+        print('2')
+    else:
+        print('1')
+
+else:
+    vraag2 = input('Duw je de man van de brug?')
+    if vraag2 == 'ja':
+        print('1')
+    else:
+        print('5')
+
+
+
+
+
+
