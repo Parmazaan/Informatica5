9193cbedec47effb78392d37a027a1653ffe7783
Parmazaan <jelle.parmentier@sgsintpaulus.eu>
2018-10-08 21:09:37 +0200

    crocodileparadox

---
+++ 06 - Condities/SchaalVanSaffir-Simpson.py
@@ -0,0 +1,19 @@
+windsnelheid = int(input('Wat is de windsnelheid? '))
+
+if windsnelheid >= 119 and windsnelheid <= 153:
+    print('categorie 1')
+
+elif windsnelheid >= 154 and windsnelheid <= 177:
+    print('categorie 2')
+
+elif windsnelheid >= 178 and windsnelheid <= 209:
+    print('categorie 3')
+
+elif windsnelheid >= 210 and windsnelheid <= 249:
+    print('categorie 4')
+
+elif windsnelheid >= 250:
+    print('categorie 5')
+
+else:
+    print('geen orkaan')
