5af4f74a574b467f97baa595560642f937b560a6
Parmazaan <jelle.parmentier@sgsintpaulus.eu>
2018-11-05 20:11:42 +0100

    rad van fortune

---
+++ 07b-Iteraties-While_Lus/Priemgetallen.py
@@ -0,0 +1,13 @@
+num = int(input(''))
+
+if num > 1:
+
+    for i in range(2, num):
+        if (num % i) == 0:
+            print(num, "is geen priemgetal")
+            break
+    else:
+        print(num, "is een priemgetal")
+
+else:
+    print(num, "is geen priemgetal")
